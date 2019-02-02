d17525953c69261e043ed141246425c72066b466
Teodor26 <teodorbear26@gmail.com>
2019-02-03 00:40:46 +0200

    Abstract class was made.

---
+++ FixedAssets.BusinessLogic/BaseForAsset.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixedAssets.BusinessLogic
+{
+    public abstract class BaseForAsset
+    {
+        public string Name { get; set; }
+
+        public DateTime Expiration { get; set; }
+    }
+}
